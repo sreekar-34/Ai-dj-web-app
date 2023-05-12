515d3978d2751141c4162d67b7b7fb35e69dfe42
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2023-05-12 14:34:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 song = "";
+
 leftWristX = 0;
 leftWristY = 0;
+
 rightWristX = 0;
 rightWristY = 0;
+
+scoreLeftWrist = 0;
 
 function preload(){
     song = loadSound("music.mp3");
@@ -11,14 +15,28 @@ function preload(){
 function setup(){
     canvas = createCanvas(600,500);
     canvas.center();
+
     video = createCapture(VIDEO);
     video.hide();
+
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
 
 function draw(){
 image(video,0,0,600,500);
+
+fill("red");
+stroke("red");
+
+if(scoreLeftWrist > 0.2){
+circle(leftWristX,leftWristY,20);
+InNumberleftWristY = Number(leftWristY);
+remove_decimals = floor(InNumberleftWristY);
+volume = remove_decimals/500;
+document.getElementById("volume").innerHTML = " Volume = " + volume;
+song.setVolume(volume);
+}
 }
 
 function play(){
@@ -34,6 +52,8 @@ function modelLoaded(){
 function gotPoses(results){
  if(results.length > 0){
     console.log(results);
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
+    
     leftWristX = results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
     console.log("leftWristX =" + leftWristX +" leftWristY = "+ leftWristY);
