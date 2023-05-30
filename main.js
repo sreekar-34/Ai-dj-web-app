c90a883e2eba67815049cccbb0dcb251442cc716
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2023-05-30 17:54:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ rightWristX = 0;
 rightWristY = 0;
 
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function preload(){
     song = loadSound("music.mp3");
@@ -28,7 +29,36 @@ image(video,0,0,600,500);
 
 fill("red");
 stroke("red");
+circle(rightWristX,rightWristY,20);
 
+if(scoreRightWrist > 0.2){
+
+if(rightWristY > 0 && rightWristY <= 100){
+    document.getElementById("speed").innerHTML = "Speed = 0.5 X";
+    song.rate(0.5);
+}
+
+else if(rightWristY > 100 && rightWristY <= 200){
+    document.getElementById("speed").innerHTML = "Speed = 1 X";
+    song.rate(1);
+}
+
+else if(rightWristY > 200 && rightWristY <= 300){
+    document.getElementById("speed").innerHTML = "Speed = 1.5 X";
+    song.rate(1.5);
+}
+
+else if(rightWristY > 300 && rightWristY <= 400){
+    document.getElementById("speed").innerHTML = "Speed = 2 X";
+    song.rate(2);
+}
+
+else if(rightWristY > 400 && rightWristY <= 500){
+    document.getElementById("speed").innerHTML = "Speed = 2.5 X";
+    song.rate(2.5);
+}
+
+}
 if(scoreLeftWrist > 0.2){
 circle(leftWristX,leftWristY,20);
 InNumberleftWristY = Number(leftWristY);
@@ -52,8 +82,13 @@ function modelLoaded(){
 function gotPoses(results){
  if(results.length > 0){
     console.log(results);
+
     scoreLeftWrist = results[0].pose.keypoints[9].score;
-    
+    scoreRightWrist = results[0].pose.keypoints[10].score;
+
+    console.log("Score Of Left Wrist = "+scoreLeftWrist);
+    console.log("Score Of Right Wrist = "+scoreRightWrist);
+
     leftWristX = results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
     console.log("leftWristX =" + leftWristX +" leftWristY = "+ leftWristY);
